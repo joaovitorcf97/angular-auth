c947ff65b8b6d91aa2ce0a73aee09f393e3cb770
João Vitor <joaovitorcfdev@gmail.com>
2023-08-13 22:25:50 -0300

    create project

---
+++ src/app/core/components/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignComponent } from './pages/sign/sign.component';
+
+@NgModule({
+  declarations: [SignComponent],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
